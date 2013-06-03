c18b7b98d698f9a66daa9ba46054ad28d55a1c3c
CLF <e0725675student.tuwien.ac.at>
2013-06-03 16:26:50 +0200

    expanded TestVBox - added EventHandler - streamlined startup (machine can already be powered up)

--- lab2/src/main/java/TestVBox.java
+++ lab2/src/main/java/TestVBox.java
@@ -14,6 +14,7 @@
  * VirtualBox OSE distribution. VirtualBox OSE is distributed in the
  * hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  */
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,70 +23,81 @@ import org.virtualbox_4_2.HWVirtExPropertyType;
 import org.virtualbox_4_2.IEvent;
 import org.virtualbox_4_2.IEventListener;
 import org.virtualbox_4_2.IEventSource;
+import org.virtualbox_4_2.IGuestPropertyChangedEvent;
 import org.virtualbox_4_2.IMachine;
 import org.virtualbox_4_2.IMachineStateChangedEvent;
+import org.virtualbox_4_2.ISessionStateChangedEvent;
 import org.virtualbox_4_2.IVirtualBox;
+import org.virtualbox_4_2.LockType;
+import org.virtualbox_4_2.MachineState;
 import org.virtualbox_4_2.VBoxEventType;
 import org.virtualbox_4_2.VBoxException;
 import org.virtualbox_4_2.VirtualBoxManager;
+import org.virtualbox_4_2.jaxws.SessionState;
 
 public class TestVBox {
-	static void processEvent(IEvent ev) {
-		System.out.println("got event: " + ev);
-		VBoxEventType type = ev.getType();
-		System.out.println("type = " + type);
-		switch (type) {
-		case OnMachineStateChanged: {
-			IMachineStateChangedEvent mcse = IMachineStateChangedEvent
-					.queryInterface(ev);
-			if (mcse == null)
-				System.out.println("Cannot query an interface");
-			else
-				System.out.println("mid=" + mcse.getMachineId());
-			break;
+	
+	static class EventHandler extends Thread {
+		private VirtualBoxManager mgr = null;
+		private IEventSource es = null;
+		private IEventListener listener = null;
+		private boolean running = true;
+		
+		EventHandler(VirtualBoxManager mgr, IEventSource es) {
+			this.mgr = mgr;
+			this.es = es;
+			listener = this.es.createListener();
+			es.registerListener(listener, Arrays.asList(VBoxEventType.Any), false);
+			System.out.println("EventHandler initialized");
 		}
-		default:
-			System.out.println("type: default");
-			break;
+		public void close() {
+			this.running = false;
+			System.out.println("Closing EventHandler");
 		}
-	}
-
-	static class EventHandler {
-		EventHandler() {
-		}
-
-		public void handleEvent(IEvent ev) {
-			try {
-				processEvent(ev);
-			} catch (Throwable t) {
-				t.printStackTrace();
-			}
-		}
-	}
-
-	static void testEvents(VirtualBoxManager mgr, IEventSource es) {
-		// active mode for Java doesn't fully work yet, and using passive
-		// is more portable (the only mode for MSCOM and WS) and thus generally
-		// recommended
-		IEventListener listener = es.createListener();
-
-		es.registerListener(listener, Arrays.asList(VBoxEventType.Any), false);
-
-		try {
-			for (int i = 0; i < 50; i++) {
-				System.out.print(".");
+		public void run() {
+			System.out.println("Starting EventHandler");
+			while(running) {
 				IEvent ev = es.getEvent(listener, 500);
 				if (ev != null) {
-					processEvent(ev);
+					handleEvent(ev);
 					es.eventProcessed(listener, ev);
 				}
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+			this.es.unregisterListener(listener);
 		}
-
-		es.unregisterListener(listener);
+		private void handleEvent(IEvent ev) {
+			System.out.println("got event: " + ev.getWrapped());
+			VBoxEventType type = ev.getType();
+			System.out.println("type = " + type);
+			switch (type) {
+			case OnMachineStateChanged: {
+				IMachineStateChangedEvent mcse = IMachineStateChangedEvent
+						.queryInterface(ev);
+				if (mcse == null)
+					System.out.println("Cannot query an interface");
+				else
+					System.out.println("mid=" + mcse.getMachineId());
+					System.out.println("State=" + mcse.getState().toString());
+				break;
+			}
+			case OnSessionStateChanged: {
+				ISessionStateChangedEvent cev = ISessionStateChangedEvent.queryInterface(ev);
+				System.out.println(cev.getState().toString());
+			}
+			
+			case OnGuestPropertyChanged: {
+				IGuestPropertyChangedEvent cev = IGuestPropertyChangedEvent.queryInterface(ev);
+				System.out.println(cev.getName());
+			}
+			default:
+				System.out.println("type: default");
+				break;
+			}
+		}
 	}
+	
+	public static EventHandler eventHandler = null;
+
 
 	static void testEnumeration(VirtualBoxManager mgr, IVirtualBox vbox) {
 		List<IMachine> machs = vbox.getMachines();
@@ -116,44 +128,33 @@ public class TestVBox {
 		}
 	}
 
+	/**
+	 * Starts the first Machine in VirtualBox
+	 * @param mgr
+	 * @param vbox
+	 */
 	static void testStart(VirtualBoxManager mgr, IVirtualBox vbox) {
 		String m = vbox.getMachines().get(0).getName();
 		System.out.println("\nAttempting to start VM '" + m + "'");
-		mgr.startVm(m, null, 7000);
-	}
-
-	static void testMultiServer() {
-		VirtualBoxManager mgr1 = VirtualBoxManager.createInstance(null);
-		VirtualBoxManager mgr2 = VirtualBoxManager.createInstance(null);
-
-		try {
-			mgr1.connect("http://i7:18083", "", "");
-			mgr2.connect("http://main:18083", "", "");
-
-			String mach1 = mgr1.getVBox().getMachines().get(0).getName();
-			String mach2 = mgr2.getVBox().getMachines().get(0).getName();
-
-			mgr1.startVm(mach1, null, 7000);
-			mgr2.startVm(mach2, null, 7000);
-		} finally {
-			mgr1.cleanup();
-			mgr2.cleanup();
+		if (!mgr.getVBox().getMachines().get(0).getState().equals(MachineState.Running)) {
+			//mgr.startVm(m, null, 7000);
+			mgr.getVBox().getMachines().get(0).lockMachine(mgr.getSessionObject(), LockType.Write);
+			System.out.println("Press Enter to start");
+			try {
+				System.in.read();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			mgr.getSessionObject().getConsole().powerUp();
 		}
+		
+		
+		//mgr.getSessionObject().unlockMachine();
 	}
 
-	static void testReadLog(VirtualBoxManager mgr, IVirtualBox vbox) {
-		IMachine m = vbox.getMachines().get(0);
-		long logNo = 0;
-		long off = 0;
-		long size = 16 * 1024;
-		while (true) {
-			byte[] buf = m.readLog(logNo, off, size);
-			if (buf.length == 0)
-				break;
-			System.out.print(new String(buf));
-			off += buf.length;
-		}
-	}
+
+	
 
 	public static void main(String[] args) {
 		VirtualBoxManager mgr = VirtualBoxManager.createInstance(null);
@@ -190,13 +191,21 @@ public class TestVBox {
 			if (vbox != null) {
 				System.out.println("VirtualBox version: " + vbox.getVersion()
 						+ "\n");
-				testEnumeration(mgr, vbox);
-				testReadLog(mgr, vbox);
+				//testEnumeration(mgr, vbox);
+				//testReadLog(mgr, vbox);
 				testStart(mgr, vbox);
-				testEvents(mgr, vbox.getEventSource());
-
+				System.out.println("Box started");
+				eventHandler = new EventHandler(mgr, vbox.getEventSource());
+				System.out.println("EventHandler created");
+				eventHandler.start();
+				System.out.println("EventHandler started");
+				System.out.println("Press Enter to stop");
+				System.in.read();
+			
+				System.out.println("Stopping EventHandler");
+				eventHandler.close();
 				System.out.println("done, press Enter...");
-				int ch = System.in.read();
+				System.in.read();
 			}
 		} catch (VBoxException e) {
 			System.out.println("VBox error: " + e.getMessage() + " original="
@@ -217,5 +226,36 @@ public class TestVBox {
 		mgr.cleanup();
 		System.out.println("Exit");
 	}
+	static void testMultiServer() {
+		VirtualBoxManager mgr1 = VirtualBoxManager.createInstance(null);
+		VirtualBoxManager mgr2 = VirtualBoxManager.createInstance(null);
 
+		try {
+			mgr1.connect("http://i7:18083", "", "");
+			mgr2.connect("http://main:18083", "", "");
+
+			String mach1 = mgr1.getVBox().getMachines().get(0).getName();
+			String mach2 = mgr2.getVBox().getMachines().get(0).getName();
+
+			mgr1.startVm(mach1, null, 7000);
+			mgr2.startVm(mach2, null, 7000);
+		} finally {
+			mgr1.cleanup();
+			mgr2.cleanup();
+		}
+	}
+
+	static void testReadLog(VirtualBoxManager mgr, IVirtualBox vbox) {
+		IMachine m = vbox.getMachines().get(0);
+		long logNo = 0;
+		long off = 0;
+		long size = 16 * 1024;
+		while (true) {
+			byte[] buf = m.readLog(logNo, off, size);
+			if (buf.length == 0)
+				break;
+			System.out.print(new String(buf));
+			off += buf.length;
+		}
+	}
 }
