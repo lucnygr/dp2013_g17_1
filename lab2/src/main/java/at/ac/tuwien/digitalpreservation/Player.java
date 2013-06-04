2fd679aecf9df1cfa66a033910ac609ce39aca66
CLF <e0725675student.tuwien.ac.at>
2013-06-04 16:22:41 +0200

    implemented player

--- lab2/src/main/java/at/ac/tuwien/digitalpreservation/Player.java
+++ lab2/src/main/java/at/ac/tuwien/digitalpreservation/Player.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.digitalpreservation;
 
 import java.io.File;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -8,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import at.ac.tuwien.digitalpreservation.config.AbstractEvent;
-import at.ac.tuwien.digitalpreservation.config.EventTypeEnum;
 import at.ac.tuwien.digitalpreservation.config.GCAP;
 import at.ac.tuwien.digitalpreservation.config.KeyboardEvent;
 import at.ac.tuwien.digitalpreservation.config.MouseEvent;
@@ -28,18 +28,17 @@ public class Player {
 	 * @param recordingName
 	 * @param vm
 	 */
-	Player(File gcap, String recordingName, VirtualMachine vm) {
-		this.name = recordingName;
+	public Player(File gcap, VirtualMachine vm) {
 		this.vm = vm;
 		
 		// load file
 		this.gcap = ConfigurationUtils.unmarshal(gcap);
 		this.recs = this.gcap.getRecording();
 	}
-	void play(String name) {
+	public void play(String name) {
 		Recording rec = null;
 		for (Recording r : recs) {
-			if (r.getDescription().equalsIgnoreCase("name")) {
+			if (r.getDescription().equalsIgnoreCase(name)) {
 				rec = r;
 			}
 		}
@@ -47,26 +46,31 @@ public class Player {
 			LOGGER.warn("No such recording \""+name+"\"");
 			return;
 		}
-		
-		//TODO
+
 		List<AbstractEvent> list = rec.getKeyboardEventOrMouseEventOrScreenshotEvent();
 		long starttime = new Date().getTime();
 		for (AbstractEvent ev : list) {
+			final long TIMEOUT = 10000;
+			long offset = 0;
+			do {
+				offset = (new Date().getTime())-starttime;
+			} while(offset < ev.getTimeOffset() || offset < TIMEOUT);
 			
-			while((new Date().getTime())-starttime < ev.getOffset()) {
-
-			}
 			switch(ev.getType()) {
 			case KEYBOARD_EVENT: {
 				vm.putKeyboardEvent((KeyboardEvent)ev);
+				break;
 			}
 			case MOUSE_EVENT: {
 				vm.putMouseEvent((MouseEvent)ev);
+				break;
 			}
 			case SCREENSHOT_EVENT: {
-				vm.takeScreenShot("test.png");
+				vm.takeScreenShot("src/test/resources/" + name+"_"+Calendar.getInstance().getTime().getTime()+".png");
+				break;
 			}
 			default:
+				LOGGER.error("Invalid Event");
 			}
 		}
 	}
