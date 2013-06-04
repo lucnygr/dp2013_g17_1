99c9751c012d8f973ccb24d2f750040effe134c0
CLF <e0725675student.tuwien.ac.at>
2013-06-04 14:35:34 +0200

    implemented commands in Starter

--- lab2/src/main/java/at/ac/tuwien/digitalpreservation/starter/Starter.java
+++ lab2/src/main/java/at/ac/tuwien/digitalpreservation/starter/Starter.java
@@ -1,5 +1,10 @@
 package at.ac.tuwien.digitalpreservation.starter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 import at.ac.tuwien.digitalpreservation.VirtualMachine;
 
 public class Starter {
@@ -8,14 +13,14 @@ public class Starter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		
-		
+
+
+
 		String vm = null;
 		String url = null;
 		String user = null;
 		String passwd = null;
-		
+
 		for (int i = 0; i < args.length; i++) {
 			if ("-vm".equals(args[i]))
 				vm = args[++i];
@@ -27,7 +32,7 @@ public class Starter {
 				passwd = args[++i];
 		}
 		if (vm == null || url == null || user == null || passwd == null) {
-			System.out.println("Not enough parameters, using default parameters.");
+			System.out.println("Not enough arguments, using default arguments.");
 			vm = "Windows XP - SP 3 - 32bit";
 			url = "127.0.0.1:18083";
 			user = "test-vb";
@@ -36,8 +41,76 @@ public class Starter {
 		System.out.println("-vm:"+vm+" url:"+url+" user:"+user+" passwd:"+passwd);
 		VirtualMachine machine = new VirtualMachine(
 				vm, user, passwd);
+		System.out.println("Starting Virtual Machine \""+vm+"\"...");
+		
 		machine.init();
+		
+		boolean running = true;
+		InputStreamReader reader = new InputStreamReader(System.in);
+		BufferedReader in = new BufferedReader(reader);
+
+		System.out.println("Ready. Type \"help\" for a list of commands");
+		while(running) {
+			String line = "";
+			try {
+				line = in.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (line.equalsIgnoreCase("exit")) {
+				running = false;
+			} else if (line.equalsIgnoreCase("help")) {
+				System.out.println("Valid commands:\n" +
+						"run <recording>\n" +
+						"record <newrecording>\n" +
+						"exit");
+				continue;
+			}
+			StringTokenizer tok = new StringTokenizer(line, " ");
+			if (!tok.hasMoreTokens()) {
+				System.out.println("Invalid command");
+				continue;
+			}
+			String command = tok.nextToken();
+			if (command.equalsIgnoreCase("run")) {
+				if (!tok.hasMoreTokens()) {
+					System.out.println("Invalid command");
+					continue;
+				}
+				
+				String name = tok.nextToken();
+				
+				playRecording(machine, name);
+				continue;
+			} else if(command.equalsIgnoreCase("record")) {
+				if (!tok.hasMoreTokens()) {
+					System.out.println("Invalid command");
+					continue;
+				}
+				
+				String name = tok.nextToken();
+				
+				record(machine, name);
+				continue;
+			} else {
+				System.out.println("Invalid command");
+				continue;
+			}
+		}
+		System.out.println("Shutting down VM...");
 		machine.destroy();
+		System.out.println("done");
+	}
+
+	static boolean record(VirtualMachine machine, String name) {
+		//TODO: implement
+		return false;
+	}
+
+	static boolean playRecording(VirtualMachine machine, String name) {
+		//TODO: implement
+		return false;
 	}
 
 }
