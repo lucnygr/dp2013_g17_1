13bfeceb071d5f4afe0880c0fb3f4ed8f0863524
Gregor Lucny <lucny.gregor@gmail.com>
2013-06-28 17:44:48 +0200

    a little bit of javadoc

--- lab2/src/main/java/at/ac/tuwien/digitalpreservation/config/EventTypeEnum.java
+++ lab2/src/main/java/at/ac/tuwien/digitalpreservation/config/EventTypeEnum.java
@@ -1,9 +1,34 @@
 package at.ac.tuwien.digitalpreservation.config;
 
+/**
+ * This enumeration contains all events that are saved in the
+ * xml-configuration-files. Each of this event is recorded or generated during
+ * the recording. It specifies the type of the event and therefore the later
+ * playback of the configuration.
+ * 
+ * @author gregor
+ * 
+ */
 public enum EventTypeEnum {
 
-	MOUSE_EVENT, KEYBOARD_EVENT, SCREENSHOT_EVENT;
+	/** A mouse event, like mouse movement or mouse clicks. */
+	MOUSE_EVENT,
+	/** A keyboard event, like pressing or releasing buttons. */
+	KEYBOARD_EVENT,
+	/**
+	 * A screenshot event. This is a generated event during playback and tells
+	 * the player that it should take a screenshot at the specified time.
+	 */
+	SCREENSHOT_EVENT;
 
+	/**
+	 * Parses the given string to the real EventTypeEnum element.
+	 * 
+	 * @param eventType
+	 *            The enum element as string.
+	 * @return The real EventTypeEnum. If the string does not match any element
+	 *         of the enumeration, return null.
+	 */
 	public static EventTypeEnum parseEventType(String eventType) {
 		for (EventTypeEnum ete : values()) {
 			if (eventType.equals(ete.toString())) {
@@ -13,6 +38,13 @@ public enum EventTypeEnum {
 		return null;
 	}
 
+	/**
+	 * Returns the toString() representation of the Enumeration element.
+	 * 
+	 * @param eventType
+	 *            The enum element.
+	 * @return a string
+	 */
 	public static String printEventType(EventTypeEnum eventType) {
 		return eventType.toString();
 	}
--- lab2/src/main/java/at/ac/tuwien/digitalpreservation/config/MouseButtonEnum.java
+++ lab2/src/main/java/at/ac/tuwien/digitalpreservation/config/MouseButtonEnum.java
@@ -4,9 +4,21 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * This enumeration elements represent the three mouse buttons left, right and
+ * middle.
+ * 
+ * @author gregor
+ * 
+ */
 public enum MouseButtonEnum {
 
-	LEFT(1), RIGHT(2), MIDDLE(4);
+	/** The left mousebutton. Internally represented as 1. */
+	LEFT(1),
+	/** The right mousebutotn. Internally represented as 2. */
+	RIGHT(2),
+	/** The right mousebutotn. Internally represented as 4. */
+	MIDDLE(4);
 
 	private int bitmap;
 
@@ -14,10 +26,22 @@ public enum MouseButtonEnum {
 		this.bitmap = bitmap;
 	}
 
+	/**
+	 * Returns the bitmap-representation of the clicked mouse button.
+	 * 
+	 * @return
+	 */
 	public int getBitmap() {
 		return bitmap;
 	}
 
+	/**
+	 * Looks if the current button is marked as clicked in the given bitmap.
+	 * 
+	 * @param bitmap
+	 *            The bitmask of the clicked buttons.
+	 * @return true if the button is clicked, otherwise false
+	 */
 	public boolean isClicked(int bitmap) {
 		int result = this.bitmap & bitmap;
 		if (result == 0) {
@@ -26,6 +50,13 @@ public enum MouseButtonEnum {
 		return true;
 	}
 
+	/**
+	 * Returns a Set of all mouse buttons marked as clicked in the given bitmap.
+	 * 
+	 * @param bitmap
+	 *            The bitmask of the clicked buttons.
+	 * @return A set with all clicked mouse buttons.
+	 */
 	public static Set<MouseButtonEnum> getClicked(int bitmap) {
 		Set<MouseButtonEnum> mbeSet = new HashSet<>();
 		for (MouseButtonEnum mbe : values()) {
@@ -36,6 +67,13 @@ public enum MouseButtonEnum {
 		return mbeSet;
 	}
 
+	/**
+	 * Returns a bitmap of all the clicked mousebuttons given as collection.
+	 * 
+	 * @param mouseButtons
+	 *            A collection of clicked mousebuttons
+	 * @return The bitmask representing the clicked buttons.
+	 */
 	public static int getClicked(Collection<MouseButtonEnum> mouseButtons) {
 		int bitmap = 0;
 		for (MouseButtonEnum mbe : mouseButtons) {
@@ -44,6 +82,14 @@ public enum MouseButtonEnum {
 		return bitmap;
 	}
 
+	/**
+	 * Parses the given string to the real MouseButtonEnum element.
+	 * 
+	 * @param eventType
+	 *            The enum element as string.
+	 * @return The real MouseButtonEnum. If the string does not match any
+	 *         element of the enumeration, return null.
+	 */
 	public static MouseButtonEnum parseMouseButton(String mouseButton) {
 		for (MouseButtonEnum mbe : values()) {
 			if (mouseButton.equals(mbe.toString())) {
@@ -53,6 +99,13 @@ public enum MouseButtonEnum {
 		return null;
 	}
 
+	/**
+	 * Returns the toString() representation of the Enumeration element.
+	 * 
+	 * @param eventType
+	 *            The enum element.
+	 * @return a string
+	 */
 	public static String printMouseButton(MouseButtonEnum mouseButton) {
 		return mouseButton.toString();
 	}
